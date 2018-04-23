f27639c257c0cc7f68bf4bd98a5d04e410bd4a64
Dmitry <dimitry.buldakov@gmail.com>
2018-04-23 06:07:44 +0300

    V14 GA

--- MyBot.java
+++ MyBot.java
@@ -1,12 +1,12 @@
 import hlt.*;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class MyBot {
 
     public static void main(final String[] args) {
         final Networking networking = new Networking();
-        final GameMap gameMap = networking.initialize("Tamagocchi");
+        final GameMap gameMap = networking.initialize("_Nag1bAt0r2OO5_");
 
         // We now have 1 full minute to analyse the initial map.
         final String initialMapIntelligence =
@@ -20,31 +20,108 @@ public class MyBot {
         for (;;) {
             moveList.clear();
             networking.updateMap(gameMap);
+            doCalculations(gameMap, moveList);
+            Networking.sendMoves(moveList);
+        }
+    }
 
-            for (final Ship ship : gameMap.getMyPlayer().getShips().values()) {
-                if (ship.getDockingStatus() != Ship.DockingStatus.Undocked) {
+    private static void doCalculations(GameMap gameMap, ArrayList<Move> moveList) {
+        Collection<Ship> ships = gameMap.getMyPlayer().getShips().values();
+        Collection<Planet> planets = gameMap.getAllPlanets().values();
+        ArrayList<Planet> usedPlanets = new ArrayList<>();
+
+        for (final Ship ship : ships) {
+            if (ship.getDockingStatus() != Ship.DockingStatus.Undocked) {
+                continue;
+            }
+
+            boolean sentToNearest = false;
+            List<Planet> nearestPlanets = getSortedPlanetsByDistance(gameMap, ship);
+
+            for (Planet planet : nearestPlanets) {
+                if (planet.isOwned()) {
                     continue;
                 }
 
-                for (final Planet planet : gameMap.getAllPlanets().values()) {
-                    if (planet.isOwned()) {
-                        continue;
-                    }
-
-                    if (ship.canDock(planet)) {
-                        moveList.add(new DockMove(ship, planet));
-                        break;
-                    }
-
-                    final ThrustMove newThrustMove = Navigation.navigateShipToDock(gameMap, ship, planet, Constants.MAX_SPEED/2);
-                    if (newThrustMove != null) {
-                        moveList.add(newThrustMove);
-                    }
-
+                if (ship.canDock(planet) && !isPlanetUsed(planet, usedPlanets)) {
+                    moveList.add(new DockMove(ship, planet));
+                    usedPlanets.add(planet);
+                    sentToNearest = true;
                     break;
                 }
             }
-            Networking.sendMoves(moveList);
+
+            if (sentToNearest) {
+                continue;
+            }
+
+            boolean sentToLargest = false;
+            List<Planet> largestPlanets = getSortedPlanetsByRadius(planets);
+
+            for (Planet planet : largestPlanets) {
+                if (planet.isOwned() && planet.getOwner() == gameMap.getMyPlayer().getId() && planet.isFull()) {
+                    continue;
+                }
+
+                if (ship.canDock(planet)) {
+                    moveList.add(new DockMove(ship, planet));
+                    sentToLargest = true;
+                    break;
+                }
+            }
+
+            if (sentToLargest) {
+                continue;
+            }
+
+            Planet planet = getNearestFreePlanet(nearestPlanets);
+
+            final ThrustMove newThrustMove = Navigation.navigateShipToDock(gameMap, ship, planet, Constants.MAX_SPEED);
+
+            if (newThrustMove != null) {
+                moveList.add(newThrustMove);
+            }
         }
+    }
+
+    private static boolean isPlanetUsed(Planet planet, ArrayList<Planet> usedPlanets) {
+        for (Planet p : usedPlanets) {
+            if (p.equals(planet)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static Planet getNearestFreePlanet(List<Planet> nearestPlanets) {
+        for (Planet planet : nearestPlanets) {
+            if (!planet.isOwned()) {
+                return planet;
+            }
+        }
+
+        return nearestPlanets.get(0);
+    }
+
+    // Get the biggest planet!
+    private static List<Planet> getSortedPlanetsByRadius(Collection<Planet> allPlanets) {
+        ArrayList<Planet> planets = new ArrayList<>(allPlanets);
+        planets.sort((o1, o2) -> Double.compare(o2.getRadius(), o1.getRadius()));
+        return planets;
+    }
+
+    private static List<Planet> getSortedPlanetsByDistance(GameMap gameMap, Ship ship) {
+        List<Planet> result = new ArrayList<>();
+
+        Map<Double, Entity> closestEntities = gameMap.nearbyEntitiesByDistance(ship);
+
+        for (Entity entity : closestEntities.values()) {
+            if (entity instanceof Planet) {
+                result.add((Planet) entity);
+            }
+        }
+
+        return result;
     }
 }
